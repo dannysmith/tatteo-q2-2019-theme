f00780b4e54be27dd2b7214bdcb8c7d3f91a9fe6
Kenny Ward <hello@kennyward.co.uk>
2019-06-11 00:20:22 +0100

    Add JS fallback for CSS vars support

--- functions.php
+++ functions.php
@@ -123,6 +123,8 @@ function tatteo_scripts() {
 	wp_enqueue_style( 'tatteo-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'tatteo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	
+	wp_enqueue_script( 'tatteo-css-vars-ponyfill', get_template_directory_uri() . '/js/css-vars-ponyfill.min.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'tatteo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
