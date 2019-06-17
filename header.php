b3a8b61885c6097d597cbac4a36fb17c1146ad8a
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-17 18:21:00 +0100

    added button to header, made simple toggle script, made search form hidden by deafult

--- header.php
+++ header.php
@@ -25,58 +25,137 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tatteo' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php if (has_custom_logo()) {
-				$custom_logo_id = get_theme_mod( 'custom_logo' );
-				$image = wp_get_attachment_image( $custom_logo_id , 'site-logo' ); ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<?php echo $image; ?>
-				</a>
-				<?php } else {
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
-				}
-			?>
-		</div><!-- .site-branding -->
+		<div class="fixed-header">
+			<div class="site-branding">
+				<?php if (has_custom_logo()) {
+					$custom_logo_id = get_theme_mod( 'custom_logo' );
+					$image = wp_get_attachment_image( $custom_logo_id , 'site-logo' ); ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<?php echo $image; ?>
+					</a>
+					<?php } else {
+						if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php endif;
+					}
+				?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-		
-		<nav class="secondary-navigation">
-			<div class="search-reveal">
-				<?php get_search_form(); ?>
+			<nav id="site-navigation" class="main-navigation">
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+
+			<nav class="secondary-navigation">
+				<div class="search-reveal">
+					<?php get_search_form(); ?>
+				</div>
+				<button class="search-button">
+				<div class="user-login">
+					<?php
+					if ( is_user_logged_in() ) {
+					$currentUser = wp_get_current_user();
+					$editUserProfile = get_edit_profile_url(); ?>
+						<a href="<?php echo $editUserProfile; ?>" class="user-avatar" style="background-image: url(<?php echo esc_url( get_avatar_url( $currentUser->ID ) ); ?>)">
+							My account
+						</a>
+					<?php } else { ?>
+						<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="Login">
+							Login
+						</a>
+					<?php } ?>
+				</div>
+				<div class="nav-toggle">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</nav>
+		</div>
+
+		<!-- Div Fixed Header -->
+		<div class="header-search-form-wrapper dark-filter search-toggle">
+			<div class="header-search-form search-toggle">
+				<form name="search-form" method="get" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<!-- LOCATION -->
+					<label for="location">Location</label>
+					<input type="text" name="location" id="location" placeholder="Search Locations...">
+					<!-- DATES -->
+					<label for="dates">Dates</label>
+					<input type="date" name="date_from" id="dates">
+					<input type="date" name="date_to" id="dates">
+					<!-- ART STYLE -->
+
+					<label for="style">Style</label>
+					<fieldset>
+						<?php
+						$art_styles = get_terms(array(
+							'taxonomy' => 'art_style',
+							'hide_empty' => false,
+						));
+						foreach ($art_styles as $art_style) {
+							$style_name = $art_style->name;
+							echo '<p>';
+							echo '<input type="checkbox" name="art_style" value="'.$style_name.'" id="'.$style_name.'">';
+							echo '<label for="'.$style_name.'">' .$style_name. '</label>';
+							echo '</p>';
+						}
+						?>
+					</fieldset>
+
+					<!-- COMISSION -->
+
+					<label for="comission">Comission</label>
+					<fieldset>
+						<?php
+						$comissions = get_terms(array(
+							'taxonomy' => 'comission',
+							'hide_empty' => false,
+						));
+						foreach ($comissions as $comission) {
+							$comission_amount = $comission->name;
+							echo '<p>';
+							echo '<input type="checkbox" name="comission" value="'.$comission_amount.'" id="'.$comission_amount.'">';
+							echo '<label for="'.$comission_amount.'">' .$comission_amount. '</label>';
+							echo '</p>';
+						}
+						?>
+					</fieldset>
+
+					<!-- TOOLS -->
+
+					<label for="tools">Tools</label>
+					<fieldset>
+						<?php
+						$tools = get_terms(array(
+							'taxonomy' => 'tools',
+							'hide_empty' => false,
+						));
+						foreach ($tools as $tool) {
+							$tool_name = $tool->name;
+							echo '<p>';
+							echo '<input type="checkbox" name="tools" value="'.$tool_name.'" id="'.$tool_name.'">';
+							echo '<label for="'.$tool_name.'">' .$tool_name. '</label>';
+							echo '</p>';
+						}
+						?>
+					</fieldset>
+					<!-- ACCOMODATION -->
+					<input type="checkbox" name="accomodation" id="accomodation">
+					<label for="accomodation">Accomodation</label>
+					<input type="submit" id="searchsubmit" value="Search" />
+				</form>
 			</div>
-			<div class="user-login">
-				<?php 
-				if ( is_user_logged_in() ) { 
-				$currentUser = wp_get_current_user();
-				$editUserProfile = get_edit_profile_url(); ?>
-					<a href="<?php echo $editUserProfile; ?>" class="user-avatar" style="background-image: url(<?php echo esc_url( get_avatar_url( $currentUser->ID ) ); ?>)">
-						My account
-					</a>
-				<?php } else { ?>
-					<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="Login">
-						Login
-					</a>
-				<?php } ?>		
-			</div>
-			<div class="nav-toggle">
-			  <span></span>
-			  <span></span>
-			  <span></span>
-			  <span></span>
-			</div>						
-			
-		</nav>
+		</div>
 	</header><!-- #masthead -->
+
+
 
 	<div id="content" class="site-content">
---
+++ js/search-form-toggle.js
@@ -0,0 +1,8 @@
+(function($) {
+	$('.search-button').on('touchstart click', function(){
+		// reveal search input
+		$('.header-search-form-wrapper').slideToggle();
+
+	});
+
+})(jQuery);
