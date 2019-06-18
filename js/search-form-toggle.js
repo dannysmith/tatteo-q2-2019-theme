09a75d466f3a23cbf29e838ae997effc7975dafa
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-18 19:21:22 +0100

    added jquery ui datepicker

--- js/search-form-toggle.js
+++ js/search-form-toggle.js
@@ -1,32 +1,40 @@
 (function($) {
-  //Shows search form on header icon click
-	$('.search-button').on('touchstart click', function(){
+	//Shows search form on header icon click
+	$('.search-button').on('touchstart click', function() {
 		$('.header-search-form-wrapper').toggle();
 	});
 
-  // Event handler to hide dropdown boxes on another input focus
-  $('#location, #dates, #accomodation').on('focus', function(){
-    $('.taxonomies-dropdown').hide()
-    $('.taxonomies-button').removeClass('button-focused')
-    //<-- Add line to hide calendar dropdowns here -->
-  })
+	// Event handler to hide dropdown boxes on another input focus
+	$('#location, #dates, #accomodation').on('focus', function() {
+		$('.taxonomies-dropdown').hide();
+		$('.taxonomies-button').removeClass('button-focused');
+		//<-- Add line to hide calendar dropdowns here -->
+	});
 
-  //Event handled for every taxonomy button
-  $('.taxonomies-button').on('touchstart click', function(event){
-    event.preventDefault();
+	$('#dates').each(function() {
+		$(this).datepicker();
+	});
 
-    //Toggles the taxonomy dropdown for the clicked button, while hiding the other forms
-    let taxonomyForms = document.querySelectorAll('.taxonomy-form')
-    taxonomyForms.forEach(function(element) {
-      if (element.contains(event.target)) {
-        $(event.target).toggleClass('button-focused')
-        $(element).find('.taxonomies-dropdown').toggle();
-      }
-      else {
-        $(element).find('.taxonomies-dropdown').hide()
-        $(element).find('.taxonomies-button').removeClass('button-focused')
-      }
-    })
-  })
+	//Event handled for every taxonomy button
+	$('.taxonomies-button').on('touchstart click', function(event) {
+		event.preventDefault();
+
+		//Toggles the taxonomy dropdown for the clicked button, while hiding the other forms
+		let taxonomyForms = document.querySelectorAll('.taxonomy-form');
+		taxonomyForms.forEach(function(element) {
+			if (element.contains(event.target)) {
+				$(event.target).toggleClass('button-focused');
+				$(element)
+					.find('.taxonomies-dropdown')
+					.toggle();
+			} else {
+				$(element)
+					.find('.taxonomies-dropdown')
+					.hide();
+				$(element)
+					.find('.taxonomies-button')
+					.removeClass('button-focused');
+			}
+		});
+	});
 })(jQuery);
-
