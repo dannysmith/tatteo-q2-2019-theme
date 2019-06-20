1afa958f205f565570ee702252ce4eb734de9304
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-20 15:31:13 +0100

    reverted header.php to origin/master

--- header.php
+++ header.php
@@ -12,9 +12,8 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -22,68 +21,62 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'tatteo'); ?></a>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tatteo' ); ?></a>
 
-		<header id="masthead" class="site-header">
-			<div class="fixed-header">
-				<div class="site-branding">
-					<?php if (has_custom_logo()) {
-						$custom_logo_id = get_theme_mod('custom_logo');
-						$image = wp_get_attachment_image($custom_logo_id, 'site-logo'); ?>
-						<a href="<?php echo esc_url(home_url('/')); ?>">
-							<?php echo $image; ?>
-						</a>
-					<?php } else {
-					if (is_front_page() && is_home()) : ?>
-							<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-						<?php else : ?>
-							<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-						<?php endif;
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php if (has_custom_logo()) {
+				$custom_logo_id = get_theme_mod( 'custom_logo' );
+				$image = wp_get_attachment_image( $custom_logo_id , 'site-logo' ); ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php echo $image; ?>
+				</a>
+				<?php } else {
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif;
 				}
-				?>
-				</div><!-- .site-branding -->
+			?>
+		</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					));
-					?>
-				</nav><!-- #site-navigation -->
-
-				<nav class="secondary-navigation">
-					<button class="search-button"><img src="<?php echo get_site_url() ?>/wp-content/themes/tatteo/img/search.svg"></button>
-					<div class="search-reveal">
-						<?php get_search_form(); ?>
-					</div>
-					<div class="user-login">
-						<?php
-						if (is_user_logged_in()) {
-							$currentUser = wp_get_current_user();
-							$editUserProfile = get_edit_profile_url(); ?>
-							<a href="<?php echo $editUserProfile; ?>" class="user-avatar" style="background-image: url(<?php echo esc_url(get_avatar_url($currentUser->ID)); ?>)">
-								My account
-							</a>
-						<?php } else { ?>
-							<a href="<?php echo wp_login_url(get_permalink()); ?>" title="Login">
-								Login
-							</a>
-						<?php } ?>
-					</div>
-					<div class="nav-toggle">
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-				</nav>
-				<?php get_template_part('template-parts/content', 'searchform') ?>
+		<nav id="site-navigation" class="main-navigation">
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+		
+		<nav class="secondary-navigation">
+			<div class="search-reveal">
+				<?php get_search_form(); ?>
 			</div>
+			<div class="user-login">
+				<?php 
+				if ( is_user_logged_in() ) { 
+				$currentUser = wp_get_current_user();
+				$editUserProfile = get_edit_profile_url(); ?>
+					<a href="<?php echo $editUserProfile; ?>" class="user-avatar" style="background-image: url(<?php echo esc_url( get_avatar_url( $currentUser->ID ) ); ?>)">
+						My account
+					</a>
+				<?php } else { ?>
+					<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="Login">
+						Login
+					</a>
+				<?php } ?>		
+			</div>
+			<div class="nav-toggle">
+			  <span></span>
+			  <span></span>
+			  <span></span>
+			  <span></span>
+			</div>						
+			
+		</nav>
+	</header><!-- #masthead -->
 
-		</header><!-- #masthead -->
-
-
-
-		<div id="content" class="site-content">
+	<div id="content" class="site-content">
