e8036f735ce47256a3254535a9e2b53d0e3f212f
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-20 17:17:49 +0100

    updated header search to toggle on click, added close icon

--- js/search-reveal.js
+++ js/search-reveal.js
@@ -1,20 +1,19 @@
 /**
  * Hide/show search input in header
  */
- 
+
 (function($) {
 	var searchContainer = $('.search-reveal'),
-	searchInput = $('.search-reveal .search-field');
-	
+		searchInput = $('.search-reveal .search-field');
+
 	// turn off autocomplete
 	searchInput.attr('autocomplete', 'off');
-	
+
 	// listen for click and touch events
-	searchContainer.on('touchstart click', function(){
+	searchInput.on('touchstart click', function() {
 		// reveal search input
-		$(searchContainer).addClass('revealed');
+		$(searchContainer).toggleClass('revealed');
 		// focus on search input
 		$(searchInput).focus();
 	});
-
 })(jQuery);
