731bc954623888d184dde6fa19729f0dfc06bf28
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-20 16:59:02 +0100

    updated search-reveal.js to toggle header search form visibility on click and change search icon to x

--- js/search-reveal.js
+++ js/search-reveal.js
@@ -1,20 +1,19 @@
 /**
  * Hide/show search input in header
  */
- 
+
 (function($) {
 	var searchContainer = $('.search-reveal'),
-	searchInput = $('.search-reveal .search-field');
-	
+		searchInput = $('.search-reveal .search-field');
+
 	// turn off autocomplete
 	searchInput.attr('autocomplete', 'off');
-	
+
 	// listen for click and touch events
-	searchContainer.on('touchstart click', function(){
+	$(searchInput).click(function() {
 		// reveal search input
-		$(searchContainer).addClass('revealed');
+		$(searchContainer).toggleClass('revealed close-button');
 		// focus on search input
 		$(searchInput).focus();
 	});
-
 })(jQuery);
