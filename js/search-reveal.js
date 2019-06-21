aff0e8ed6cb1e25e4be61176f84bf53821453ac9
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-21 14:08:46 +0100

    fixed search form closing on in put click

--- js/search-reveal.js
+++ js/search-reveal.js
@@ -10,10 +10,14 @@
 	searchInput.attr('autocomplete', 'off');
 
 	// listen for click and touch events
-	searchInput.on('touchstart click', function() {
+	searchInput.click(function() {
 		// reveal search input
 		$(searchContainer).toggleClass('revealed');
 		// focus on search input
 		$(searchInput).focus();
+	});
+	searchContainer.click(function() {
+		// reveal search input
+		$(searchContainer).toggleClass('revealed');
 	});
 })(jQuery);
