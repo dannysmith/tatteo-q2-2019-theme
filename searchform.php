f97cb64ed0548988319ba00e7624b0ac6936e62f
Jen <vobis.jennifer@yahoo.co.uk>
2019-06-21 11:11:16 +0100

    took out searchform.php and directly included the search form in 404.php

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <input type="text" value="" name="s" id="s" placeholder="..try another search" />
-        <input type="submit" id="searchsubmit" value="Submit" />
-    </div>
-</form>
