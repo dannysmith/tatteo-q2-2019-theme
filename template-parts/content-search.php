25cc8316eba03731e3cfc6fb411a16d92e27d5b4
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-24 11:44:36 +0100

    added replacement text if no image set

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,18 +11,22 @@ $post_type = get_post_type(get_the_ID());
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<a href=" <?php echo get_the_permalink() ?>" class="search-result-wrapper">
-		<!-- Replace with image field -->
-		<img src="<?php echo the_field() ?>alt=" featured image of the <?php echo $post_type ?>">
+		<?php if (!empty(get_field())); { ?>
+			<!-- Replace with image field -->
+			<img src="<?php echo the_field(); ?>alt=" featured image of the <?php echo $post_type ?>">
+		<?php } else {
+		echo '<p No photos yet </p>';
+	} ?>
 		<div class="search-result-text">
 			<div class="search-result-header">
 				<!-- Replace with name field -->
-				<h2><?php echo get_field() ?></h2>
+				<h2><?php echo get_field(); ?></h2>
 				<!-- Replace with location field -->
-				<h3><?php echo get_field() ?></h3>
+				<h3><?php echo get_field(); ?></h3>
 			</div>
 			<div class="search-result-footer">
 				<!-- Replace with comission term -->
-				<p><?php echo get_field() ?></p>
+				<p><?php echo get_field(); ?></p>
 				<!-- Replace with rating -->
 				<p></p>
 			</div>
