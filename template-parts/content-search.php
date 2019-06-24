7124dfd374ae410f05f9d218225875304c7339a1
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-24 11:19:58 +0100

    replaced content-template search, styled accordingly, need to add custom fields

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,14 +11,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<a href=" <?php echo get_the_permalink() ?>" class="search-result-wrapper">
+		<!-- Replace with image field -->
 		<img src='https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500'>
 		<div class="search-result-text">
 			<div class="search-result-header">
+				<!-- Replace with name field -->
 				<h2>Old Habits</h2>
+				<!-- Replace with location field -->
 				<h3>Kingsland, London</h3>
 			</div>
 			<div class="search-result-footer">
+				<!-- Replace with comission term -->
 				<p>10% Comission</p>
+				<!-- Replace with rating -->
 				<p> *****</p>
 			</div>
 		</div>
