ec9dca8164945e1312496ee87ab319fc3b7855a0
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-24 10:58:57 +0100

    initial commit

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,26 +10,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			tatteo_posted_on();
-			tatteo_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php tatteo_post_thumbnail(); ?>
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php tatteo_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<a href=" <?php echo get_the_permalink() ?>" class="search-result-wrapper">
+		<img src='https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500'>
+		<div class="search-result-text">
+			<div class="search-result-header">
+				<h2>Old Habits</h2>
+				<h3>Kingsland, London</h3>
+			</div>
+			<div class="search-result-footer">
+				<p>10% Comission</p>
+				<p> *****</p>
+			</div>
+		</div>
+	</a>
 </article><!-- #post-<?php the_ID(); ?> -->
