b367668f2a5ef333e18cf5cbf350a4d0aabfa9d6
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-24 11:30:05 +0100

    removed placeholders

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -12,19 +12,19 @@ $post_type = get_post_type(get_the_ID());
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<a href=" <?php echo get_the_permalink() ?>" class="search-result-wrapper">
 		<!-- Replace with image field -->
-		<img src='https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500' alt="featured image of the <?php echo $post_type ?>">
+		<img src="<?php echo the_field() ?>alt=" featured image of the <?php echo $post_type ?>">
 		<div class="search-result-text">
 			<div class="search-result-header">
 				<!-- Replace with name field -->
-				<h2>Old Habits</h2>
+				<h2><?php echo get_field() ?></h2>
 				<!-- Replace with location field -->
-				<h3>Kingsland, London</h3>
+				<h3><?php echo get_field() ?></h3>
 			</div>
 			<div class="search-result-footer">
 				<!-- Replace with comission term -->
-				<p>10% Comission</p>
+				<p><?php echo get_field() ?></p>
 				<!-- Replace with rating -->
-				<p> *****</p>
+				<p></p>
 			</div>
 		</div>
 	</a>
