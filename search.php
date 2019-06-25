f6676aed628561226e7fd2bb60fcab1926669a78
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-06-25 17:41:05 +0100

    typo

--- search.php
+++ search.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-<?php get_template_part('template-parts', 'attributefilter') ?>
+<?php get_template_part('template-parts/content', 'attributefilter') ?>
 <section id="primary" class="content-area">
 	<main id="main" class="site-main">
 
