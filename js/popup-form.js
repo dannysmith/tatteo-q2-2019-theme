cad7963ed7aca81c3f12a98f9289c6e2b22e2fca
Jen <vobis.jennifer@yahoo.co.uk>
2019-06-26 13:01:41 +0100

    changed toggle to fade in/out

--- js/popup-form.js
+++ js/popup-form.js
@@ -1,12 +1,12 @@
 (function ($) {
   $(".open").on("click", function () {
-    $(".popup-overlay").toggle('easing');
+    $(".popup-overlay").fadeIn();
   });
 })(jQuery);
 
 
 (function ($) {
   $(".close-form").on("click", function () {
-    $(".popup-overlay").toggle('fast');
+    $(".popup-overlay").fadeOut();
   });
 })(jQuery);
