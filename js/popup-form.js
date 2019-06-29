513ceb0373851e2d18007210147ff116a2b9ccbf
Kenny Ward <hello@kennyward.co.uk>
2019-06-29 22:50:28 +0100

    change strings to singlequotes

--- js/popup-form.js
+++ js/popup-form.js
@@ -1,12 +1,12 @@
 (function ($) {
-  $(".open").on("click", function () {
-    $(".popup-overlay").fadeIn();
+  $('.open').on('click', function () {
+    $('.popup-overlay').fadeIn();
   });
 })(jQuery);
 
 
 (function ($) {
-  $(".close-form").on("click", function () {
-    $(".popup-overlay").fadeOut();
+  $('.close-form').on('click', function () {
+    $('.popup-overlay').fadeOut();
   });
 })(jQuery);
