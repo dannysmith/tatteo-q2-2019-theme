a852d55322f541460c9eeae524d2144e3a45ee51
Kenny Ward <hello@kennyward.co.uk>
2019-10-12 01:40:44 +0100

    Fix titles on homepage for mobile

--- front-page.php
+++ front-page.php
@@ -18,10 +18,7 @@ get_header();
 			the_post();
 
 			get_template_part('template-parts/content', 'hero');
-			if (is_user_logged_in()) :
-				get_template_part('template-parts/content', 'front-page');
-			endif;
-
+			get_template_part('template-parts/content', 'front-page');
 
 		/* If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
